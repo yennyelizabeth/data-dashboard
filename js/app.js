a16363e81b329241dceb38e79f976d5e56a9eda1
AraceliGS <aragutarras@gmail.com>
2017-11-28 16:19:10 -0500

    añadiendo eventos relacionados al modal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 /*
  * Funcionalidad de tu producto
  */
-
+window.onload = function() {
+  var menu = document.getElementsByClassName('fa-bars')[0];
+  var script = document.getElementsByTagName('script')[0];
+  menu.addEventListener('click', function showModal(event) {
+    var modal = document.createElement('div');
+    var body = document.body;
+    modal.classList.add('modal');
+    body.appendChild(modal);
+    body.insertBefore(modal, script);
+  });
+};
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
