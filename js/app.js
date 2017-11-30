2cb2c102ae4b480e6a6554ca9f0ebbf32a59957c
AraceliGS <aragutarras@gmail.com>
2017-11-30 09:36:38 -0500

    añadiendo avances donde obtengo la puntuacion promedio de los jedi, los teachers, las estudiantes satisfechas y el nps

--- js/app.js
+++ js/app.js
@@ -9,6 +9,11 @@ window.addEventListener('load', function(event) {
   var select = document.getElementById('navbar');
   var sedes = Object.keys(data); // AQ, CDM, LM, CH
   var generations = Object.values(data);
+  var score5 = document.getElementById('nps');
+  var promotersContainer = document.getElementById('promoters');
+  var passiveContainer = document.getElementById('passive');
+  var detractorsContainer = document.getElementById('detractors');
+  var score8 = document.getElementById('satisfied-students');
   var score9 = document.getElementById('average-score-for-teachers');
   var score10 = document.getElementById('average-score-for-jedi');
   select.addEventListener('click', function(event) {
@@ -25,15 +30,54 @@ window.addEventListener('load', function(event) {
         arrayOfGenerations = Object.values(data)[0];
         specificGeneration = arrayOfGenerations[generation];
         var ratings = specificGeneration['ratings'];
-        /* Variable para almacenar la suma de toda las puntuaciones a los jedi*/
+        /* Variables*/
+        /* Para el punto 5 */
+        var totalPromoters = 0;
+        var totalPassive = 0;
+        var totalDetractors = 0;
+        /* Para el punto 8 */
+        var dontMeet = 0;
+        var meet = 0;
+        var exceed = 0;
+        /* Para el punto 9 */
+        var averageScoreForTeachers = 0;
+        /* Para el punto 10 */
         var averageScoreForJedi = 0;
+        /* Recorriendo con un for el object ratings */
         for (var i = 0; i < ratings.length; i++) {
-          /* Obteniendo la puntuación de los jedi*/
+          /* Obteniendo el Net Promoter Score (NPS) promedio de los sprints cursados. */
+          totalPromoters += ratings[i]['nps']['promoters'];
+          totalPassive += ratings[i]['nps']['passive'];
+          totalDetractors += ratings[i]['nps']['detractors'];
+          /* Obteniendo el porcentaje de estudiantes satisfechas con la experiencia de Laboratoria. */
+          dontMeet += ratings[i]['student']['no-cumple'];
+          meet += ratings[i]['student']['cumple'];
+          exceed += ratings[i]['student']['supera'];
+          /* Obteniendo la puntuación de los jedi */
           var jediScore = ratings[i]['jedi'];
           averageScoreForJedi += jediScore;
+          /* Obteniendo la puntuación de los teachers*/
+          var teacherScore = ratings[i]['teacher'];
+          averageScoreForTeachers += teacherScore;
         }
+        /* El Net Promoter Score (NPS) promedio de los sprints cursados */
+        debugger;
+        var promoters = totalPromoters * 100;
+        var passive = totalPassive * 100;
+        var detractors = totalDetractors * 100;
+        var nps = promoters - detractors;
+        score5.textContent = nps + '%';
+        /* Porcentaje de estudiantes satisfechas */
+        var total = dontMeet + meet + exceed;
+        var happyStudents = meet + exceed;
+        var satisfiedStudents = (happyStudents * 100) / total;
+        score8.textContent = satisfiedStudents;
+        /* console.log(satisfiedStudents); */
+        /* Puntuación promedio de los teachers */
+        var averageScore2 = Math.round(averageScoreForTeachers / 4);
+        score9.textContent = averageScore2;
         /* Puntuación promedio de los jedi */
-        var averageScore = parseInt(averageScoreForJedi / 4);
+        var averageScore = Math.round(averageScoreForJedi / 4);
         // alert(averageScore);
         score10.textContent = averageScore;
         // console.log(averageScore);
